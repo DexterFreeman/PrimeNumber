71c920820c4e6a6de8e8fafdf98102f6e30c3088
FREEMAN Dexter (Consulting For Edenred) <Dexter.FREEMAN@pps.edenred.com>
2023-06-15 15:59:27 +0100

    quick reformat

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,31 +6,28 @@ public class Main {
 
     private final static Scanner scanner = new Scanner(System.in);
 
-    public static void primeNumberApplication(){
+    public static void primeNumberApplication() {
         System.out.println();
         System.out.println("Input a number to see if it is a prime number:");
         System.out.println("Or type quit to close the application.");
         String userInput = scanner.next();
         //To lower case so that the user input on "quit" isn't case sensitive.
-        switch (userInput.toLowerCase(Locale.ROOT)){
-            case "quit":
-                System.out.println("Thank you for using the application.");
-                break;
-            default:
-                try{
-                    if (userInput.length() >= 10){
-                        BigInteger strToBigInt = new BigInteger(userInput);
-                        System.out.println(Math.isPrime(strToBigInt));
-                    }
-                    else {
-                        int userInputToInteger = Integer.parseInt(userInput);
-                        System.out.println(Math.isPrime(userInputToInteger));
-                    }
+        if ("quit".equals(userInput.toLowerCase(Locale.ROOT))) {
+            System.out.println("Thank you for using the application.");
+        } else {
+            try {
+                //int max value is 2147483647 which is 10 length.
+                if (userInput.length() >= 10) {
+                    BigInteger strToBigInt = new BigInteger(userInput);
+                    System.out.println(Math.isPrime(strToBigInt));
+                } else {
+                    int userInputToInteger = Integer.parseInt(userInput);
+                    System.out.println(Math.isPrime(userInputToInteger));
                 }
-                catch (Exception e){
-                    System.out.println("Error; input was not valid, make sure the input is either a number or quit");
-                }
-                primeNumberApplication();
+            } catch (Exception e) {
+                System.out.println("Error; input was not valid, make sure the input is either a number or quit");
+            }
+            primeNumberApplication();
         }
     }
 
